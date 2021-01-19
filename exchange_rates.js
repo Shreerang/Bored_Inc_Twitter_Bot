45b8aaeb5cc3af33e52e70957adb32833a2c6d4c
Shreerang Patwardhan <spatwardhan@spatwardhan-mbp.local>
2021-01-18 16:31:43 -0800

    Updated config key usage

--- exchange_rates.js
+++ exchange_rates.js
@@ -4,10 +4,10 @@ const axios = require("axios");
 const tweet = require("./thread.js");
 
 const T = new Twit({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret,
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000,
 });
 
